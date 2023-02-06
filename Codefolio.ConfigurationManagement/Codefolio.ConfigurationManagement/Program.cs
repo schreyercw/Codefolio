60ad1d5b5005552ca159f068177f49bf72c8a66f
Charles Schreyer <cwschreyer@gmail.com>
2023-02-06 18:48:39 +1300

    Added first instance of the configuration managment project, a simple CLI with DI hooks to create a good foundational base layer for future projects.

---
+++ Codefolio.ConfigurationManagement/Codefolio.ConfigurationManagement/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+//Construct the Builder
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appConfig.json")
+    .Build();
+
+//Instantiate the Service collection
+IServiceCollection services = new ServiceCollection();
+
+
+//Build the service provider
+var serviceProvider = services.BuildServiceProvider();
+
+//Discover the services
+
