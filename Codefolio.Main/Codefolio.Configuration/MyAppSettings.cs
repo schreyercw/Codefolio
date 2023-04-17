3a79092c837aeb2081ee78fe5c6933cbce9f4157
Charles Schreyer <cwschreyer@gmail.com>
2023-04-17 22:03:14 +1200

    Updated Configuration to only include the configuration setup without DI and fixed secrets

---
+++ Codefolio.Main/Codefolio.Configuration/MyAppSettings.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codefolio.Configuration
+{
+    public class MyAppSettings
+    {
+        public MySecrets MySecrets { get; set; }
+    }
+
+    public class MySecrets
+    {
+        public string? MyDbConnection { get; set; }
+        public string? MyApiKey { get; set; }
+    }
+}
