96366e84327a0c88c73a13974e92c32e190d772c
schreyercw <122328948+schreyercw@users.noreply.github.com>
2023-04-17 22:08:56 +1200

    Updated Configuration to only include the configuration setup without DI and fixed secrets (#3)

---
+++ Codefolio.Main/Codefolio.Configuration/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using System.Net.Http;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using Codefolio.Configuration;
+
+var configuration = new ConfigurationBuilder()
+.AddJsonFile("appsettings.json", false)
+.AddUserSecrets(typeof(Program).Assembly, false)
+.Build();
+
+var myAppSettingsOptions = new OptionsWrapper<MyAppSettings>(new MyAppSettings());
+
+configuration.Bind(myAppSettingsOptions.Value);
+var myAppSettings = myAppSettingsOptions.Value;
+
+Console.WriteLine(myAppSettings.MySecrets.MyDbConnection);
+Console.WriteLine(configuration["MySecrets:MyDbConnection"]);
+
+Console.Read();
