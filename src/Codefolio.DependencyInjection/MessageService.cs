69e787021e42a397f1ff9130a83e5ee3238a540e
Charles Schreyer <cwschreyer@gmail.com>
2023-04-18 10:32:29 +1200

    Added Dependancy Injection initial with first iteration

---
+++ src/Codefolio.DependencyInjection/MessageService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codefolio.DependencyInjection
+{
+    public class MessageService
+    {
+        private readonly IMessageSender _sender;
+
+        public MessageService(IMessageSender sender)
+        {
+            _sender = sender;
+        }
+
+        public void SendMessage(string message)
+        {
+            _sender.SendMessage(message);
+        }
+    }
+}
---
+++ src/Codefolio.DependencyInjection/Program.cs
@@ -0,0 +1,27 @@
+﻿using Codefolio.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create a new service collection
+        var services = new ServiceCollection();
+
+        // Register the IMessageSender implementations as singleton services
+        services.AddSingleton<IMessageSender, EmailSender>();
+        services.AddSingleton<IMessageSender, SmsSender>();
+
+        // Register the MessageService class as a transient service
+        services.AddTransient<MessageService>();
+
+        // Build the service provider
+        var serviceProvider = services.BuildServiceProvider();
+
+        // Resolve a MessageService instance from the service provider
+        var messageService = serviceProvider.GetService<MessageService>();
+
+        // Use the MessageService instance to send a message using one of the IMessageSender implementations
+        messageService.SendMessage("Hello, world!");
+    }
+}
