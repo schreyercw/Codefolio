d4725e15d52ff0766990687ede3737c72ad89e86
Charles Schreyer <cwschreyer@gmail.com>
2023-04-17 20:46:01 +1200

    Added first iteration of configuration

---
+++ Codefolio.Main/Codefolio.Configuration/IOptions.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codefolio.Configuration
+{
+    internal class IOptions
+    {
+    }
+
+    public class MyAppSettings
+    {
+        public string? AllowedHosts { get; set; }
+        public LogLevel LogLevel { get; set; }
+    }
+
+    public class MySecrets
+    {
+        public string? MyDbConnection { get; set; }
+        public string? MyApiKey { get; set; }
+    }
+
+    public class MyController : ControllerBase
+    {
+        private readonly MyAppSettings _appSettings;
+        private readonly MySecrets _secrets;
+
+        public MyController(IOptions<MyAppSettings> appSettings, IOptions<MySecrets> secrets)
+        {
+            _appSettings = appSettings.Value;
+            _secrets = secrets.Value;
+        }
+
+        public IActionResult MyAction()
+        {
+            // Use the configuration settings here...
+            return Ok();
+        }
+    }
+}
